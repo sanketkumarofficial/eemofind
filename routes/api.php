7d00818bb59bf6d12bb6ee671bce4df4536f95fd
Sanket Kumar <ssbkumar5513@gmail.com>
2026-06-17 13:11:40 +0530

    sanket

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
+
 use App\Http\Controllers\Api\MobileController;
+use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::prefix('v1')->group(function () {
     Route::post('auth/register', [AuthController::class, 'register'])->middleware('throttle:10,1');
